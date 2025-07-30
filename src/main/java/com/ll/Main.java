2348e5718e86c6a9d7914506d6eb1f3ee281e8fd
Park-seungg <parkis1101@gmail.com>
2025-07-30 10:29:03 +0900

    요구사항 4단계 -1

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -4,28 +4,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("== 명언 앱 ==");
 
+        System.out.println("== 명언 앱 ==");
         Scanner scanner = new Scanner(System.in);
+        int wiseSayingId = 0;
 
         while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine().trim();
-
             if (cmd.equals("종료")) {
-                System.out.println("프로그램을 종료합니다.");
+                System.out.println("프로그램을 종료합니다");
                 break;
-            } else  if (cmd.equals("등록")) {
-                System.out.print("명언: ");
-                String wiseSayingContent = scanner.nextLine().trim();
-
-                System.out.print("작가: ");
-                String wiseSayingAuthor = scanner.nextLine().trim();
-
-                System.out.println("1번 명언이 등록되었습니다.");
+            } else if (cmd.equals("등록")) {
+                System.out.print("명언 : ");
+                String wise = scanner.nextLine().trim();
+                System.out.print("작가 : ");
+                String auth = scanner.nextLine().trim();
+                wiseSayingId++;
+                System.out.println(wiseSayingId + "번 명언이 등록되었습니다.");
             }
         }
-
-        scanner.close();
     }
 }
